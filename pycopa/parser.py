d35b366a946ef60f0604cd0c9506b4f74d6186a7
Advaita Krishna das <advaita.krishna.das@gmail.com>
2017-06-09 16:35:17 +0300

    code cleanup

--- pycopa/parser.py
+++ pycopa/parser.py
@@ -1,30 +1,3 @@
-import re
-
-
-class Lexer:
-    def parse(self, line):
-        words = re.split(r'(\s+)', line)
-        result = []
-
-        for word in words:
-            if word.startswith("/"):
-                result.append((word, "command"))
-                continue
-
-            if ":" in word:
-                ts = word.split(":")
-                result.append((ts[0], "arg"))
-                if ts[1]:
-                    result.append((ts[1], "string"))
-                continue
-
-            if word:
-                result.append((word, "string"))
-
-
-        return result
-
-
 class Parser:
     __DEFAULT_ARG_NAME = "arg"
 
@@ -58,4 +31,3 @@ class Parser:
             result[arg] = result[arg].strip()
 
         return result
-
