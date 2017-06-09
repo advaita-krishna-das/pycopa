b9a2d703a860f5b81b5866122c367c0868208568
Advaita Krishna das <advaita.krishna.das@gmail.com>
2017-06-09 19:11:58 +0300

    fix

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # pycopa
 A dead simple python module to parse command line arguments
+
+```py
+from pycopa import parse
+parse("/say This is a test number:one done:true")
+
+# result
+{ "command": "say",
+  "arg": "This is a test",
+  "number": "one", "done": "true" }
+```
--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from distutils.core import setup
 setup(
   name = 'pycopa',
   packages = ['pycopa'],
-  version = '0.1',
+  version = '0.2.1',
   description = 'A dead simple python module to parse command line arguments',
   author = 'Advaita Krishna das',
   author_email = 'advaita.krishna.das@gmail.com',
   url = 'https://github.com/advaita-krishna-das/pycopa',
-  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
   keywords = ['command line'], # arbitrary keywords
   classifiers = [],
 )
